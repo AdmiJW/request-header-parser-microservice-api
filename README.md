c7f07e7d7f7620d13ed8d33567b45787eb31e4e6
AdmiJW <jamweg22@gmail.com>
2021-07-25 23:33:18 +0800

    Readme Update and HelmetJS

---
+++ README.md
@@ -0,0 +1,20 @@
+# 📑 Request Header Parser Microservice Project 📑
+
+Freecodecamp's Request Header Parser Microservice Project made by AdmiJW
+
+---
+<br>
+
+## Usage
+
+Clone the repository. Run `npm i` and create respective [.env](#Environment-Variables) file. Run `npm start`.
+
+---
+<br>
+
+## Environment Variables
+
+| Environment Variable | Description |
+|-|-|
+| `NODE_ENV` | `production` or `development` |
+| `PORT` | Port to run express application on. Default is `3000` |
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ require('dotenv').config()
 const path = require('path');
 const express = require('express');
 const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
 const app = express();
 
-
+//===========================
+// Rate Limiting
+//===========================
 const staticRateLimiter = rateLimiter({
     windowMs: 1000 * 60 * 5,            // 5 Min, 300 second
-    max: 300                            // Maximum of 300 request for 5 min
+    max: 300                            // Maximum of 200 request for 5 min
 });
 const apiRateLimiter = rateLimiter({
     windowMs: 1000 * 60 * 5,            // 5 Min, 300 second
@@ -19,15 +22,24 @@ const apiRateLimiter = rateLimiter({
 
 app.use('/public', staticRateLimiter, express.static('./public') );
 
-//  Resolve CORS issue if the request is from some place
-//  If the environment file does not have DISABLE XORIGIN specified
+// HelmetJS for improved security
+app.use(helmet({
+    contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+            scriptSrc: ["'self'", 'unpkg.com', 'kit.fontawesome.com'],
+            connectSrc: ["'self'", "fontawesome.com", "*.fontawesome.com"]
+        }
+    }
+}));
+
+//  FreeCodeCamp's CORS
 if (!process.env.DISABLE_XORIGIN) {
     //  Apply the origin allowing middleware
     app.use(function(req, res, next) {    
       var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];   //  Universal allowed origin
       var origin = req.headers.origin || '*';                                               //  Use headers origin, or * otherwise
       if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){           //  if environment does not specify XORIG_RESTRUCT, or origin is indeed in universal allowed origin
-           console.log(origin);
            res.setHeader('Access-Control-Allow-Origin', origin);
            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       }
@@ -36,6 +48,9 @@ if (!process.env.DISABLE_XORIGIN) {
 }
 
 
+//===========================
+// Routes
+//===========================
 app.get('/', apiRateLimiter, (req,res)=> {
     res.sendFile( path.join(__dirname, '/views/index.html') );
 });
@@ -55,5 +70,6 @@ app.get('/api/whoami', apiRateLimiter, (req,res)=> {
 
 
 app.listen( process.env.PORT || 3000, ()=> {
-    console.log("Request Header Parser Microservice started on port " + (process.env.PORT || 3000) )
+    console.log("Request Header Parser Microservice started on port " + (process.env.PORT || 3000) );
+    console.log("Mode: " + process.env.NODE_ENV);
 });
